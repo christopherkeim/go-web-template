cc799bba1553ddbe855fb7c255537f4599b12b7e
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-02-27 21:37:48 -0500

    Added data store instantiation.

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,12 +3,13 @@ package server
 import "github.com/go-chi/chi/v5"
 
 func setRoutes(router *chi.Mux) {
+	db := initDb()
 	// GET /
 	router.Get("/", GetRoot)
 	// GET /users/{guid}
-	router.Get("/users/{guid}", GetSingleUser)
+	router.Get("/users/{guid}", GetSingleUser(&db))
 	// GET /users
-	router.Get("/users", GetAllUsers)
+	router.Get("/users", GetAllUsers(&db))
 	// POST /users
-	router.Post("/users", CreateNewUser)
+	router.Post("/users", CreateNewUser(&db))
 }
