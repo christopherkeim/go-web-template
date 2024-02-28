a19302f3de31cf31533d8a4417d5e966cbbbf384
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2024-02-27 21:37:34 -0500

    Added closures for passing in a data store instance.

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -4,71 +4,82 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-)
 
-// Helper function for loading data store
-func withDb() Db {
-	return initDb()
-}
+	"github.com/go-chi/chi/v5"
+)
 
 func GetRoot(writer http.ResponseWriter, request *http.Request) {
 	writer.Write([]byte("Hello 🦫 🚀 ✨\n"))
 }
 
-func GetSingleUser(writer http.ResponseWriter, request *http.Request) {
+func GetSingleUser(db *Db) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 
-	staticUser := User{
-		Guid:      "abcd123",
-		FirstName: "Bob",
-		LastName:  "Smith",
-		Age:       31,
-		Admin:     false,
+		guid := chi.URLParam(request, "guid")
+
+		if guid == "" {
+			writer.Write([]byte(fmt.Sprintf("No input guid: %d", http.StatusNotFound)))
+			return
+		}
+
+		var foundUser User
+		for _, user := range *db {
+			if guid == user.Guid {
+				foundUser = user
+				break
+			}
+		}
+
+		jsonResponse, jsonError := json.Marshal(foundUser)
+		if jsonError != nil {
+			fmt.Printf("JSON Error: %s", jsonError.Error())
+			return
+		}
+
+		writer.Write([]byte(jsonResponse))
 	}
-
-	jsonResponse, jsonError := json.Marshal(staticUser)
-	if jsonError != nil {
-		fmt.Printf("JSON Error: %s", jsonError.Error())
-		return
-	}
-
-	writer.Write([]byte(jsonResponse))
 
 }
 
-func GetAllUsers(writer http.ResponseWriter, request *http.Request) {
-	users := initDb()
+func GetAllUsers(db *Db) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
 
-	jsonResponse, jsonError := json.Marshal(users)
-	if jsonError != nil {
-		fmt.Printf("JSON Error: %s", jsonError.Error())
-		return
+		jsonResponse, jsonError := json.Marshal(*db)
+		if jsonError != nil {
+			fmt.Printf("JSON Error: %s", jsonError.Error())
+			return
+		}
+
+		writer.Write([]byte(jsonResponse))
 	}
-
-	writer.Write([]byte(jsonResponse))
 
 }
 
-func CreateNewUser(writer http.ResponseWriter, request *http.Request) {
-	// Parse the body and extract the GopherData properties
-	request.Body = http.MaxBytesReader(writer, request.Body, 1048576)
-	decoder := json.NewDecoder((request.Body))
-	var user User
-	parsingError := decoder.Decode(&user)
+func CreateNewUser(db *Db) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		// Parse the body and extract the GopherData properties
+		request.Body = http.MaxBytesReader(writer, request.Body, 1048576)
+		decoder := json.NewDecoder((request.Body))
+		var user User
+		parsingError := decoder.Decode(&user)
 
-	if parsingError != nil {
-		fmt.Printf("Parsing Error: %s", parsingError.Error())
-		return
+		if parsingError != nil {
+			fmt.Printf("Parsing Error: %s", parsingError.Error())
+			return
+		}
+
+		*db = append(*db, user)
+
+		// Construct the validation response
+		validationResponse := UserCreationResponse{UserGuid: user.Guid, State: "success"}
+		jsonResponse, jsonError := json.Marshal(validationResponse)
+
+		if jsonError != nil {
+			fmt.Printf("JSON Error: %s", jsonError.Error())
+			return
+		}
+
+		// Send the response back to the client
+		writer.Write([]byte(jsonResponse))
 	}
-
-	// Construct the validation response
-	validationResponse := UserCreationResponse{UserGuid: user.Guid, State: "success"}
-	jsonResponse, jsonError := json.Marshal(validationResponse)
-
-	if jsonError != nil {
-		fmt.Printf("JSON Error: %s", jsonError.Error())
-		return
-	}
-
-	// Send the response back to the client
-	writer.Write([]byte(jsonResponse))
 }
